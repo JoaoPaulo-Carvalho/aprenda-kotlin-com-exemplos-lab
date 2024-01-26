4d5306e64d0d93bfc4a61b8cfa4f6bf9b04fb633
JoaoPaulo-Carvalho <41268753+JoaoPaulo-Carvalho@users.noreply.github.com>
2024-01-26 10:56:26 -0300

    Feat: renomeando enum DIFICIL para avançado; criando classe Aluno, que estende Usuario; adicionando atributos 'nome' e 'email' à classe Usuario; adicionando o atributo 'nivel' do tipo Nivel à classe Formacao; utilizando MutableSet para designar 'inscritos' da classe Formacao; método 'matricular'; função 'realizaMatricula' encapsula matrícula e verificação do resultado; casos de exemplo (teste) na 'main()'.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,37 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Aluno(nome: String, email: String) : Usuario(nome = nome, email = email)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableSetOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(aluno: Aluno): Boolean {
+        return inscritos.add(aluno)
     }
 }
 
+fun realizaMatricula(formacao: Formacao, aluno: Aluno) {
+    val resultado = formacao.matricular(aluno)
+    if (resultado) println("Aluno ${aluno.nome} matriculado com sucesso!")
+    else println("Aluno ${aluno.nome} já está matriculado!")
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aluno1 : Aluno = Aluno("John Doe", "example@domain.com")
+    val aluno2 : Aluno = Aluno("Jane Austin", "austin@domain.com")
+    
+    val ce1 = ConteudoEducacional("Orientação a Objetos em Kotlin", 90)
+    val ce2 = ConteudoEducacional("Collections em Kotlin")
+    
+    val conteudos = listOf(ce1, ce2)
+    val formacao = Formacao("Kotlin para Backend", Nivel.BASICO, conteudos)
+    
+    realizaMatricula(formacao, aluno1)
+    realizaMatricula(formacao, aluno2)
+    realizaMatricula(formacao, aluno1)
 }
